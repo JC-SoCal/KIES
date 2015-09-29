f1faa99d5834f804575a784cc9b5953ff0be84b6
JC <john.carruthers0@gmail.com>
2015-09-29 09:52:07 -0600

    added firefox popup

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
-sudo apt-get upgrade
+
+#install elasticsearch
 sudo apt-get install python-software-properties -y
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
@@ -9,6 +10,7 @@ sudo dpkg -i elasticsearch-1.7.2.deb
 rm elasticsearch-1.7.2.deb
 sudo /etc/init.d/elasticsearch start
 
+#install kibana
 wget https://download.elastic.co/kibana/kibana/kibana-4.1.2-linux-x64.tar.gz
 tar -zxvf kibana-4.1.2-linux-x64.tar.gz
 rm kibana-4.1.2-linux-x64.tar.gz
@@ -21,3 +23,7 @@ sudo wget https://raw.githubusercontent.com/JC-SoCal/KIES/master/kibana4
 sudo chmod +x /etc/init.d/kibana4
 sudo update-rc.d kibana4 defaults 96 9
 sudo service kibana4 start
+
+#launch the fruits of our labor
+sleep 1
+nohup firefox -new-tab http://127.0.0.1:9200 -new-tab http://127.0.0.1:5601 &
